e5f4160c7546724a3fcfaa92a7b80172365cebf6
Yash <mvmoraiyashraj10d@gmail.com>
2026-07-17 10:53:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i<n; i++){
-            int c=0;
-            for(int j =0; j<n;j++){
-                if(nums[j]==nums[i]){
-                    c++;
-                }
+        // for(int i = 0; i<n; i++){
+        //     int c = 0;
+        //     for(int j =0; j<n;j++){
+        //         if(nums[j]==nums[i]){
+        //             c++;
+        //         }
+        //     }
+        //     if(c==1) return nums[i];
+        // }
+        // return 0;
+        int maxx = nums[0];
+        for(int i = 1; i<n; i++){
+            maxx= max(maxx, nums[i]);
+        }
+        // int hash[maxx]={0};
+        vector<int>hash(maxx+1,0);
+        for(int i = 0 ; i<n; i++){
+            hash[nums[i]]++;
+        }
+        for(int i = 0; i<maxx+1; i++){
+            if(hash[i]==1){
+                return i;
             }
-            if(c==1) return nums[i];
         }
         return 0;
     }
