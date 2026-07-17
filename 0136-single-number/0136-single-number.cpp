9ca2fb34b54844108894855f3b45933f31365b07
Yash <mvmoraiyashraj10d@gmail.com>
2026-07-17 10:54:03 +0530

    Time: 16 ms (8.28%), Space: 24 MB (7.62%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i<n; i++){
-            int c = 0;
-            for(int j =0; j<n;j++){
-                if(nums[j]==nums[i]){
-                    c++;
-                }
-            }
-            if(c==1) return nums[i];
-        }
-        return 0;
+        // for(int i = 0; i<n; i++){
+        //     int c = 0;
+        //     for(int j =0; j<n;j++){
+        //         if(nums[j]==nums[i]){
+        //             c++;
+        //         }
+        //     }
+        //     if(c==1) return nums[i];
+        // }
+        // return 0;
 
         // int maxx = nums[0];
         // for(int i = 1; i<n; i++){
@@ -29,20 +29,20 @@ public:
         // }
         // return 0;
 
-        // unordered_map<int, int> frequencies;
+        unordered_map<int, int> frequencies;
         
-        // // Step 1: Count the frequency of each number
-        // for (int num : nums) {
-        //     frequencies[num]++;
-        // }
+        // Step 1: Count the frequency of each number
+        for (int num : nums) {
+            frequencies[num]++;
+        }
         
-        // // Step 2: Find the number that appeared exactly once
-        // for (auto& pair : frequencies) {
-        //     if (pair.second == 1) {
-        //         return pair.first;
-        //     }
-        // }
+        // Step 2: Find the number that appeared exactly once
+        for (auto& pair : frequencies) {
+            if (pair.second == 1) {
+                return pair.first;
+            }
+        }
         
-        // return 0;
+        return 0;
     }
 };
